749dd91f743dfffe808ce38a41d4501c89929458
Fernando Henrique <nandohfernandes@gmail.com>
2019-10-27 17:14:55 -0300

    refactor: project strucure as cookiecutter ds

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A data science project on collection, cleaaning,manipulating and evaluating data regarding house prices.',
+    author='fernando henrique fernandes',
+    license='BSD-3',
+)
