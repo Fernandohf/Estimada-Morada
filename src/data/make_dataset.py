93d55ab861c51c15ebc8c419c5dfc25d3ab80c81
Fernando Henrique Fernandes <nandohfernandes@gmail.com>
2019-10-29 22:00:24 -0300

    fix import for make_dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,19 +1,23 @@
 """Processing steps of the dataset"""
 # -*- coding: utf-8 -*-
-import click
 import logging
-from halo import Halo
-from pathlib import Path
+import pandas as pd
 from dotenv import find_dotenv, load_dotenv
 from .scrape_data import navigate
 from .improve_and_clean import remove_duplicates_and_na, remove_outliers
-import pandas as pd
+from .utils import in_ipynb
+if in_ipynb():
+    from halo import HaloNotebook as Halo
+else:
+    from halo import Halo
 
 
-@click.command()
-@click.option('--scrape', type=click.BOOL, default=False,
-              help='Wheather or not scrape the data from the urls on "/reference/urls.txt"')
-def process_dataset(scrape, nomatin):
+# @click.command()
+# @click.option('--scrape', type=click.BOOL, default=False,
+#               help='Wheather or not scrape the data from the urls on "/reference/urls.txt"')
+# @click.argument('input_file', type=click.Path())
+# @click.argument('output_file', type=click.Path())
+def process_dataset(input_file, output_file, scrape):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
@@ -23,37 +27,36 @@ def process_dataset(scrape, nomatin):
     # Scrape data
     if scrape:
         spinner.start("Scraping data")
-        with open('../../references/urls.txt', 'r') as f:
+        with open('../references/urls.txt', 'r') as f:
             urls = f.read_lines()
         scraped_dfs = []
         for url in urls:
             scraped_dfs.append(navigate(url, 1, 500))
         # Save results
         raw_data = pd.concat(scraped_dfs)
-        raw_data.to_csv("../../data/raw/SCRAPED_DATA.csv", index=False)
+        raw_data.to_csv(input_file, index=False)
         spinner.succeed("Data Scrapped!")
     else:
-        raw_data = pd.read_csv("./data/raw/SCRAPED_DATA.csv")
+        raw_data = pd.read_csv(input_file)
 
     # Remove duplicates
     spinner.start("Removing duplicates and invalid values")
     interim_data = remove_duplicates_and_na(raw_data)
-    interim_data.to_csv("./data/interim/1_CLEAN_DATA.csv", index=False)
-    spinner.succeed("First cleaning stage done!")
+    interim_data.to_csv("../data/interim/1_CLEAN_DATA.csv", index=False)
+    spinner.succeed("Done removing duplicates!")
 
     # Remove outliers
     spinner.start("Removing outliers and inconsistent values")
     final_data = remove_outliers(interim_data)
-    final_data.to_csv("./data/processed/CLEAN_DATA.csv", index=False)
-    spinner.succeed("Final cleaning stage done!")
+    final_data.to_csv(output_file, index=False)
+    spinner.succeed("Done removing outliers!")
+    spinner.start("Cleaning processing done!")
+    spinner.stop_and_persist(symbol='✔'.encode('utf-8'), text="Cleaning processing done!")
 
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
 
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,21 +1,25 @@
 """Add new features to the dataset"""
-import click
+# import click
 import pandas as pd
 import logging
-from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 from .address_to_coordenates import apply_nomatin
-from halo import Halo
+from .utils import in_ipynb
+if in_ipynb():
+    from halo import HaloNotebook as Halo
+else:
+    from halo import Halo
 
-@click.option('--nomatin', type=click.BOOL, default=False,
-              help='Wheather or not call the nomatin API to convert the addresses')
-def add_features(nomatin):
+
+# @click.option('--nomatin', type=click.BOOL, default=False,
+#               help='Wheather or not call the nomatin API to convert the addresses')
+def add_features(input_file, nomatin):
     """ Runs build features scripts to turn processed data from (../processed) into
         improved data (saved in ../processed as well).
     """
     spinner = Halo(text='Building features...', spinner='dots')
     # Add lat/lon columns
-    clean_data = pd.read_csv("./data/processed/CLEAN_DATA.csv")
+    clean_data = pd.read_csv(input_file)
     if nomatin:
         spinner.start("Adding Latitude and Longitude columns")
         transformed_data = apply_nomatin(clean_data)
@@ -29,11 +33,6 @@ if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
 
     add_features()
